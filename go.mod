71cf4ba32e403656d17310fc52bd754fe9eea19d
gagik <gagik@princeton.edu>
2021-08-14 23:16:34 -0400

    Added authenticated mail API.

--- go.mod
+++ go.mod
@@ -7,10 +7,14 @@ require (
 	github.com/chris-ramon/douceur v0.2.0 // indirect
 	github.com/coreos/go-oidc/v3 v3.0.0
 	github.com/form3tech-oss/jwt-go v3.2.3+incompatible
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.3.0
-	github.com/microcosm-cc/bluemonday v1.0.15 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.15
 	github.com/rs/cors v1.8.0
+	github.com/sendgrid/rest v2.6.4+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.10.0+incompatible
+	gopkg.in/auth0.v5 v5.19.2 // indirect
 	gopkg.in/cas.v2 v2.2.2
 )
--- handlers/mail.go
+++ handlers/mail.go
@@ -1,10 +1,18 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
+	"hoagie-profile/auth"
 	"net/http"
+	"os"
+	"strings"
 
 	jwtmiddleware "github.com/auth0/go-jwt-middleware"
 	"github.com/gorilla/mux"
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 const (
@@ -12,11 +20,76 @@ const (
 	mailSendRoute = "/mail/send"
 )
 
+// BlueMonday sanitizes HTML, preventing unsafe user input
+var p = bluemonday.UGCPolicy()
+
 func setupMailHandlers(r *mux.Router, m *jwtmiddleware.JWTMiddleware) {
 	// Handle mail send request
-	r.Handle(mailSendRoute, m.Handler(sendHandler)).Methods("GET")
+	r.Handle(mailSendRoute, m.Handler(sendHandler)).Methods("POST")
+}
+
+type MailRequest struct {
+	Header string
+	Sender string
+	Body   string
+	Email  string
+}
+
+func sendMail(req MailRequest) {
+	from := mail.NewEmail(req.Sender, "hoagie@princeton.edu")
+	replyTo := mail.NewEmail(req.Sender, req.Email)
+	subject := req.Header
+
+	content := mail.NewContent("text/html", req.Body)
+
+	to := mail.NewEmail("Hoagie", "hoagie@princeton.edu")
+
+	message := mail.NewV3MailInit(from, subject, to, content)
+	message.SetReplyTo(replyTo)
+
+	tos := []*mail.Email{
+		mail.NewEmail("Hoagie", os.Getenv("HOAGIE_TEST_EMAIL")),
+	}
+
+	p := mail.NewPersonalization()
+	p.AddTos(tos...)
+
+	message.AddPersonalizations(p)
+
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	response, err := client.Send(message)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(response)
+	}
 }
 
 var sendHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("test"))
+	accessToken := strings.TrimPrefix(r.Header.Get("authorization"), "Bearer ")
+
+	user, err := auth.GetUser(accessToken)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var mailReq MailRequest
+	err = json.NewDecoder(r.Body).Decode(&mailReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	mailReq.Body = p.Sanitize(mailReq.Body)
+	mailReq.Body += `
+	<hr />
+	<div style="font-size:8pt;">This email was sent instantly to all 
+	college listservs with <a href="https://mail.hoagie.io/">mail.hoagie.io</a>. 
+	You can use it to automatically send emails to all students without 
+	the need to forward it to friends.</div>
+	`
+	mailReq.Email = user
+
+	sendMail(mailReq)
 })
