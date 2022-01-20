b5da2c11d97e0c1f3bfdb5ee6b36d229527be458
Edmund <edmund666@princeton.edu>
2022-01-19 21:36:59 -0500

    feat: Added validation for fields in digest

--- handlers/mail_digest.go
+++ handlers/mail_digest.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strings"
 	"time"
+	"unicode/utf8"
 
 	"go.mongodb.org/mongo-driver/bson"
 )
@@ -37,6 +38,23 @@ var digestSendHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Req
 		return
 	}
 	// Validation here
+	// Category
+	if digestReq.Category != "Lost and found" && digestReq.Category != "Sale" {
+		http.Error(w, "Category must be lost and found or sale", http.StatusBadRequest)
+		return
+	}
+
+	// Title length
+	if utf8.RuneCountInString(digestReq.Title) < 5 || utf8.RuneCountInString(digestReq.Title) > 100 {
+		http.Error(w, "Title needs to be between 5 and 200 characters inclusive.", http.StatusBadRequest)
+		return
+	}
+
+	// Description Length
+	if utf8.RuneCountInString(digestReq.Description) < 5 || utf8.RuneCountInString(digestReq.Description) > 200 {
+		http.Error(w, "Description needs to be between 5 and 200 characters inclusive.", http.StatusBadRequest)
+		return
+	}
 
 	// Add the digest request to the user's digest queue
 	db.InsertOne(client, "apps", "mail", bson.D{
