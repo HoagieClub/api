2fa1246025f43312f339bb9b96a6ceb066751bf3
Danielpark1239 <Danielpark1239@gmail.com>
2023-05-12 01:21:29 -0400

    fixed typo

--- handlers/helpers.go
+++ handlers/helpers.go
@@ -12,7 +12,7 @@ func getUser(authorizationHeader string) (user auth.User, success bool) {
 	user, err := auth.GetUser(accessToken)
 	if os.Getenv("HOAGIE_MODE") == "debug" {
 		user = auth.User{Email: "meatball@princeton.edu", Name: "Meatball Hoagie"}
-	} else if err != nil{
+	} else if err != nil {
 		return auth.User{}, false
 	}
 	return user, true
